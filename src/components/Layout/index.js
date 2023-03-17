1848511f31849b5fd4bb74433113cbe4eb4917a4
Cleaninja <pswchttopwdm@gmail.com>
2023-03-16 18:53:06 -0700

    [Update] Layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import "./layout.css";
-export default function Layout({ children = <></> }) {
+export default function Layout({ children = <></>, on = false }) {
   return (
     <div>
       <Header />
-      <div className="pt-44 px-4 md:px-8 lg:px-12 xl:px-container">{children}</div>
+      <div
+        className={`pt-44 ${on ? "px-4 md:px-8 lg:px-12 xl:px-container" : ""}`}
+      >
+        {children}
+      </div>
       <Footer />
     </div>
   );
