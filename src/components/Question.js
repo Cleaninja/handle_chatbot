0440ca43b59c66d670716743b2495ec7cf7c7753
Cleaninja <pswchttopwdm@gmail.com>
2023-03-18 08:41:56 -0700

    [Remove] clear question.css

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,12 @@
 import { DownOutlined, UpOutlined } from "@ant-design/icons";
 import React, { useState } from "react";
-import "./question.css";
 
 export default function Question({ title, children }) {
   const [isOpen, setOpen] = useState(false);
 
   return (
     <div className="bg-white rounded-md shadow-md mt-2 p-2">
-      <div className="accordion" onClick={() => setOpen(!isOpen)}>
+      <div className="font-semibold text-xl ml-3 py-2" onClick={() => setOpen(!isOpen)}>
         {title}
         {isOpen ? (
           <UpOutlined className="text-center text-lg float-right pr-10" />
