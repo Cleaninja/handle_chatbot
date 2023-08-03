9fa560f72bd76eeab158a78aa64ef0b072bece22
dmoon16 <pswchttopwdm@gmail.com>
2023-08-03 22:08:52 +0900

    [Update] Landing page front

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,31 +1,11 @@
-import { Checkbox, Col, Input, Rate, Row, Slider, Space } from "antd";
+import { Col, Input, Rate, Row, Space } from "antd";
 import React from "react";
 import Layout from "../../components/Layout";
-import "./home.css";
 import Question from "../../components/Question";
-
-const questionInputs = [
-  {
-    question: "What kind of talent?",
-    note: "Nibh elit lacus mi elit, dui maecenas vestibulum cursus. Aliquet quam cursus tortor eu a. Enim, integer pellentesque sagittis lectus aliquam sed cursus tortor, ac. Ornare quisque ullamcorper a eleifend fringilla turpis.",
-  },
-  {
-    question: "What additional language should the applicant know?",
-    note: "Nibh elit lacus mi elit, dui maecenas vestibulum cursus. Aliquet quam cursus tortor eu a. Enim, integer pellentesque sagittis lectus aliquam ",
-  },
-  {
-    question: "WORK LOCATION",
-    note: "Nibh elit lacus mi elit, dui maecenas vestibulum cursus. Aliquet quam cursus tortor eu a. Enim, integer pellentesque sagittis lectus aliquam sed cursus tortor, ac. Ornare quisque ullamcorper a eleifend fringilla turpis.",
-  },
-  {
-    question: "Where are people being recruited?",
-    note: "Nibh elit lacus mi elit, dui maecenas vestibulum cursus. Aliquet quam cursus tortor eu a. Enim, integer pellentesque sagittis lectus aliquam sed cursus tortor, ac. Ornare quisque ullamcorper a eleifend fringilla turpis.",
-  },
-  {
-    question: "What benefits does the company / job offer?",
-    note: "Nibh elit lacus mi elit, dui maecenas vestibulum cursus. Aliquet quam cursus tortor eu a. Enim, integer pellentesque sagittis lectus aliquam sed cursus tortor, ac. Ornare quisque ullamcorper a eleifend fringilla turpis.",
-  },
-];
+import BookApplication from "./BookApplication";
+import "./home.css";
+import Industry from "./Industry";
+import CustomerTestimonials from "./CustomerTestimonials";
 
 const faqs = [
   {
@@ -117,95 +97,13 @@ export default function Home() {
         </Row>
       </div>
 
-      <div className="w-100% max-w-[1600px] mx-auto">
-        <img src="./assets/imgs/image4.png" alt="banner" />
-      </div>
+      <BookApplication />
 
-      {/* Book Applicants Online */}
-      <div className="max-w-[1600px] mx-auto p-4 md:p-8 lg:p-10 xl:p-20">
-        <div className="md:grid grid-cols-10 gap-14">
-          <div className="col-span-6 pb-[34px]">
-            <h1
-              style={{ fontFamily: "Ubuntu-bold" }}
-              className="leading-normal font-extrabold text-left text-5xl md:text-6xl md:text-[70px]"
-            >
-              Book Applicants Online
-            </h1>
-            <p
-              style={{ fontFamily: "Ubuntu-bold" }}
-              className="pt-3 text-xl font-normal leading-[30px]"
-            >
-              Select the right package and enter the requirements for the
-              applicants.
-            </p>
-          </div>
-          <div className="col-span-4"></div>
-          <div className="col-span-6">
-            <div className=" text-center">
-              <p className="pt-8 pb-2 text-2xl font-normal">
-                Drag the slider 👇 to build your own bundle of
-              </p>
-              <div className="px-8">
-                <Slider defaultValue={30} />
-              </div>
-              <p className="pt-4 text-2xl font-normal">
-                $ 6,150 for 25 4,920 for = $ 246 197 per post
-              </p>
-              <p className="pt-2 text-2xl font-normal">
-                You save $ 1,230 (20% discount)
-              </p>
-            </div>
-          </div>
-          <div className="col-span-4 flex items-center flex-row">
-            <button className="rounded-lg border-2 border-solid border-[#524CF6] bg-[#524CF6] py-4 w-full">
-              <p className="px-4 text-xl font-medium text-white leading-5">
-                Buy for € 1’254.00
-              </p>
-            </button>
-          </div>
-          <div className="col-span-6">
-            <Space size="large" direction="vertical">
-              {questionInputs.map((item, index) => (
-                <Space.Compact key={index}>
-                  <QuestionInput question={item.question} note={item.note} />
-                </Space.Compact>
-              ))}
-            </Space>
-
-            <div className="mt-20">
-              <div className="grid grid-cols-12 gap-3">
-                <Checkbox className="col-span-8">
-                  Get premium support and help with your job post +€10/applicant
-                </Checkbox>
-                <div className="col-span-4 flex items-center justify-between">
-                  <p className="text-sm font-normal">Cell Text</p>
-                  <div>
-                    <p className="text-xs rounded-[10px] bg-[#f2f4f8] px-[10px]">
-                      Badge
-                    </p>
-                  </div>
-                </div>
-                <Checkbox className="col-span-8">
-                  Get premium support and help with your job post +$99 Senior
-                  Designer
-                </Checkbox>
-                <div className="col-span-4 flex items-center justify-between">
-                  <p className="text-sm font-normal">Cell Text</p>
-                  <div>
-                    <p className="text-xs rounded-[10px] bg-[#f2f4f8] px-[10px]">
-                      Badge
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      {/* Industry */}
 
       <div className="bg-[#ccd3e1] p-2">
-        <img className="w-full" src="./assets/imgs/image3.png" alt="img" />
-        <img className="w-full mt-3" src="./assets/imgs/image5.png" alt="img" />
+        <Industry />
+        <CustomerTestimonials />
       </div>
 
       {/* FAQ */}
