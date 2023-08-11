9abefb33add54eab006679a2e1e84f9c7b771084
dmoon16 <pswchttopwdm@gmail.com>
2023-08-11 22:00:40 +0200

    [change] ipaddress

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -19,7 +19,7 @@ export default function Home() {
   }, []);
 
   const getPublicKey = async () => {
-    const res = await axios.post("http://localhost:4242/get-stripe-pk");
+    const res = await axios.post("http://161.97.87.53:4242/get-stripe-pk");
     setPublicKey(res?.data?.publicKey ?? "");
   };
 
